2b16985a11eb733c3307617aedee2f94bc9eb3e1
Adam <jsj0828@yahoo.com.tw>
2020-11-07 20:29:19 +0800

    (+) 加入測試單元及測試 Stock_InfoService Function

---
+++ StockInformationTests/Models/Service/Stock_InfoServiceSqliteTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StockInformation.Models.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StockInformation.Models.DBModel;
+namespace StockInformation.Models.Service.Tests
+{
+    [TestClass()]
+    public class SqliteTest
+    {
+        [TestMethod()]
+        public void CRUD()
+        {
+            Stock_InfoService srv = new Stock_InfoService();
+
+            Stock_info test = new Stock_info()
+            {
+                Code = "test9999",
+                Name = "測試用",
+                Date = "20201107",
+                Devidend_year = 108,
+                Dividend_yield = 9.99,
+                PB_rate = 8.88,
+                PE_rate = 7.77,
+                Fiscal_year_quarter = "109/3"
+            };
+            int rtn = srv.Insert(test);
+            Assert.IsTrue(rtn > 0);
+
+            var query = srv.Query(test.Code, test.Date);
+            Assert.IsNotNull(query);
+
+            test.Name += "_update";
+            rtn = srv.Update(test);
+            Assert.IsTrue(rtn > 0);
+
+            rtn = srv.Delete(test);
+            Assert.IsTrue(rtn > 0);
+        }
+    }
+}
