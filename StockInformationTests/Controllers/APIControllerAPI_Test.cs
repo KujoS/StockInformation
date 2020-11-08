b1981d14487aba6a19cd5e0265d271c5a8c9cc4f
Adam <jsj0828@yahoo.com.tw>
2020-11-08 13:19:56 +0800

    (+) 加入QueryStockInfo 測試單元

---
+++ StockInformationTests/Controllers/APIControllerAPI_Test.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StockInformation.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using StockInformation.Models.APIModel;
+using Newtonsoft.Json;
+
+namespace StockInformation.Controllers.Tests
+{
+    [TestClass()]
+    public class APIControllerAPI_Test
+    {
+        [TestMethod()]
+        public void QueryStockInfoQueryStockInfo()
+        {
+            QueryController api = new QueryController();
+
+            var resutl = (ContentResult)api.QueryStockInfo(new Model_QueryStockInfo.Request { Code = "1101", LastDays = 1 });
+            var resp = JsonConvert.DeserializeObject<Model_QueryStockInfo.Response>(resutl.Content);
+
+
+            Assert.IsNotNull(resp);
+        }
+    }
+}
